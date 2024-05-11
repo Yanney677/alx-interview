7386d84c03862ec1bdaa42313cbe5a3bf817430a
Yanney677 <w.yanney1991@gmail.com>
2024-05-11 01:21:53 +0000

    Island Perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,7 +2,8 @@
 """Island Perimeter"""
 
 def island_perimeter(grid):
-    """Function that returns the perimeter of the given island"""
+    """Function that returns the perimeter
+    of the given island"""
     perimeter = 0
     for row in grid + list(map(list, zip(*grid))):
         for i, j in zip([0] + row, row + [0]):
